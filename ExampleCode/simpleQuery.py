01e83bf5deab82f72e7867d9496deeffeaee597f
klumb <kmlumbard@gmail.com>
2025-05-22 08:04:34 -0500

    added example code

---
+++ ExampleCode/simpleQuery.py
@@ -0,0 +1,31 @@
+import requests
+
+url = 'https://api.github.com/graphql'
+api_token = "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" # Replace with your actual token
+query = '''{
+repository(owner: "kubernetes", name: "kubernetes"){
+    pullRequests(first: 100) {
+      pageInfo{
+        hasNextPage
+        endCursor
+      }
+      edges {
+        node {
+          url
+          title
+          state
+          createdAt
+          closedAt
+          participants{
+            totalCount
+          } changedFiles additions deletions
+        } 
+      }
+    }
+  }
+}''' 
+
+json = { 'query' : query}
+headers = {'Authorization': 'token %s' % api_token}
+r = requests.post(url=url, json=json, headers=headers)
+print (r.text)
